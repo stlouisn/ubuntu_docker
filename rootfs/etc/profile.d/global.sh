590dd764099e6a85a413e395312928a870552b18
stlouisn <stlouisn@hotmail.com>
2018-01-19 20:24:38 -0400

    Create global.sh

---
+++ rootfs/etc/profile.d/global.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Default editor
+export EDITOR='/bin/nano'
+
+# Default aliases
+alias l='ls -lA --color=auto'
+alias ll='ls -l --color=auto'
+alias ls='ls --color=auto'
+alias tree='tree --dirsfirst -C'

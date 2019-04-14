8bf2e3e61270a1dcf1167df485100abe1937ec2c
stlouisn <stlouisn@hotmail.com>
2019-04-14 11:10:09 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -3,6 +3,12 @@
 # Login into docker
 echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
 
+# Build and push ARM32 image
+
+# Build and push ARMHF image
+
+# Build and push ARM64 image
+
 # Build and push AMD64 image
 buildctl build \
 	--frontend dockerfile.v0 \
@@ -11,4 +17,4 @@ buildctl build \
 	--local dockerfile=. \
 	--local context=. \
 	--output type=tar,dest=ubuntu-image.tar
-docker import ubuntu-image.tar --message "${DOCKER_NAME}:${DOCKER_TAG}" ${DOCKER_USERNAME}/${DOCKER_NAME}:latest-arm32v7
+docker import ubuntu-image.tar --message "${DOCKER_NAME}:${DOCKER_TAG}" ${DOCKER_USERNAME}/${DOCKER_NAME}:latest-amd64

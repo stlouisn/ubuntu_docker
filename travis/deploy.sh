13565b801984ec06066e8ff1c8484313d1f9036b
stlouisn <stlouisn@hotmail.com>
2019-04-14 19:44:03 -0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 
 # Login into docker
-echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
+echo ${DOCKER_PASSWORD} \| docker login --username ${DOCKER_USERNAME} --password-stdin
 
 # Create manifest list
 docker manifest create ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \

1a3d5f1cb234c0cd42f0d16d1c0abf3e06fd3dc0
stlouisn <stlouisn@hotmail.com>
2019-04-14 12:00:51 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -8,15 +8,29 @@ export DOCKER_CLI_EXPERIMENTAL="enabled"
 # Login into docker
 echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
 
+architectures="arm32 arm64 amd64"
+
+#for arch in $architectures
+#do
+#	buildctl build \
+#		--frontend dockerfile.v0 \
+#		--opt platform=linux/$arch \
+#		--opt filename=./Dockerfile.$arch \
+#		--local dockerfile=. \
+#		--local context=. \
+#		--output type=oci,dest=${DOCKER_TAG}-$arch.tar
+#	docker import ${DOCKER_TAG}-$arch.tar --message "${DOCKER_NAME}:${DOCKER_TAG}-$arch" ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-$arch
+#done
+
 # Build and push ARM32 image
 buildctl build \
 	--frontend dockerfile.v0 \
-	--opt platform=linux/arm32 \
-	--opt filename=./Dockerfile.arm32 \
+	--opt platform=linux/arm \
+	--opt filename=./Dockerfile.arm \
 	--local dockerfile=. \
 	--local context=. \
-	--output type=oci,dest=${DOCKER_TAG}-arm32.tar
-docker import ${DOCKER_TAG}-arm32.tar --message "${DOCKER_NAME}:${DOCKER_TAG}-arm32" ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32
+	--output type=oci,dest=${DOCKER_TAG}-arm.tar
+docker import ${DOCKER_TAG}-arm.tar --message "${DOCKER_NAME}:${DOCKER_TAG}-arm" ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm
 
 # Build and push ARMHF image
 

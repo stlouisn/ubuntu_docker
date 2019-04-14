c16e5464227c0d1ec765e9c586aa7d85c33b8584
stlouisn <stlouisn@hotmail.com>
2019-04-14 11:45:30 -0300

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -10,19 +10,19 @@ echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-
 
 # Create manifest list
 docker manifest create ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32 \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm64 \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32 \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm64 \
 	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-amd64
 
 # Annotate manifest list
-docker manifest annotate \
-	--arch arm --os linux \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32
-docker manifest annotate \
-	--arch arm64 --os linux \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
-	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm64
+#docker manifest annotate \
+#	--arch arm --os linux \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32
+#docker manifest annotate \
+#	--arch arm64 --os linux \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \
+#	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm64
 docker manifest annotate \
 	--arch amd64 --os linux \
 	${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG} \

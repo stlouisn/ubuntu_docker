13fe451b6627eeabc605602a9ca5e84505f54a7d
stlouisn <stlouisn@hotmail.com>
2019-04-14 11:48:33 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -9,6 +9,14 @@ export DOCKER_CLI_EXPERIMENTAL="enabled"
 echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
 
 # Build and push ARM32 image
+buildctl build \
+	--frontend dockerfile.v0 \
+	--opt platform=linux/arm32 \
+	--opt filename=./Dockerfile.arm32 \
+	--local dockerfile=. \
+	--local context=. \
+	--output type=oci,dest=${DOCKER_TAG}-arm32.tar
+docker import ${DOCKER_TAG}-arm32.tar --message "${DOCKER_NAME}:${DOCKER_TAG}-arm32" ${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-arm32
 
 # Build and push ARMHF image
 

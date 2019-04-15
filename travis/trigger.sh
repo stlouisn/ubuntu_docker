fbf1a1406b606eb1164e024c1e774d85dff0d8c7
stlouisn <stlouisn@hotmail.com>
2019-04-15 12:24:46 -0300

    Update trigger.sh

--- travis/trigger.sh
+++ travis/trigger.sh
@@ -1,9 +1,10 @@
-# List of builds to trigger
+# Builds to trigger
 builds="java_docker python_docker mono_docker traefik_docker openvpn_docker"
 
-# Trigger builds
 for build in $builds
 do
+
+  # Trigger build
   curl -X POST \
     -H "Content-Type: application/json" \
     -H "Travis-API-Version: 3" \
@@ -11,4 +12,5 @@ do
     -H "Authorization: token ${TRAVIS_API_TOKEN}" \
     -d '{"request": {"message": "Push from ${DOCKER_USERNAME}/${DOCKER_NAME}", "branch": "master"}}' \
     'https://api.travis-ci.org/repo/${DOCKER_USERNAME}%2F$build/requests'
+
 done

010c08c45d3eb0452ee7127b9073f5e8b9ac04a2
stlouisn <stlouisn@hotmail.com>
2019-04-19 18:30:40 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -5,8 +5,7 @@ set -euo pipefail
 # Architectures to build
 architectures="arm arm64 amd64"
 
-for arch in $architectures
-do
+for arch in $architectures; do
 
 	# Build temporary image
 	buildctl build \

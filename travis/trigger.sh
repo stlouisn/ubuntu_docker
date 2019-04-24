fb4626c3efa57e92380550c3eb2897e053facda9
stlouisn <stlouisn@hotmail.com>
2019-04-24 18:45:31 -0300

    Update trigger.sh

--- travis/trigger.sh
+++ travis/trigger.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Builds to trigger
-builds="java_docker mono_docker python_docker builder_docker openvpn_docker pi-hole_dockertraefik_docker"
+builds="java_docker mono_docker python_docker builder_docker openvpn_docker pi-hole_docker traefik_docker"
 
 # Commit Message
 body='{

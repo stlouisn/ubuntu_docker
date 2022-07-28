e4d7ceb486e9eee4ebf894c3cffc5fa9bc93ba4c
stlouisn <stlouisn@hotmail.com>
2022-07-28 12:50:30 -0300

    Update get_version.sh

--- .scripts/get_version.sh
+++ .scripts/get_version.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # OS version
-export OS_CODENAME="$(curl -fsSL https://raw.githubusercontent.com/tianon/docker-brew-ubuntu-core/master/latest)"
-export OS_VERSION="$(curl -fsSL --retry 5 --retry-delay 2 http://releases.ubuntu.com/${OS_CODENAME}/ | grep title | awk -F ' ' {'print $2'})"
+OS_CODENAME="$(curl -fsSL https://raw.githubusercontent.com/tianon/docker-brew-ubuntu-core/master/latest)"
+OS_VERSION="$(curl -fsSL --retry 5 --retry-delay 2 http://releases.ubuntu.com/${OS_CODENAME}/ | grep title | awk -F ' ' {'print $2'})"
 
 # Container version
-export C_VERSION="${OS_VERSION}"
-echo script done
+C_VERSION="${OS_VERSION}"
+echo "${C_VERSION}"

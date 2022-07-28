a60d73dbd7b4ea435ef18575ec750b2e01815ac4
stlouisn <stlouisn@hotmail.com>
2022-07-28 13:38:24 -0300

    Update get_version.sh

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -6,11 +6,17 @@ set -euo pipefail
 OS_CODENAME="$(curl -fsSL https://raw.githubusercontent.com/tianon/docker-brew-ubuntu-core/master/latest)"
 OS_VERSION="$(curl -fsSL --retry 5 --retry-delay 2 http://releases.ubuntu.com/$OS_CODENAME/ | grep title | awk -F ' ' {'print $2'})"
 
-# # App version
-# APP_VERSION="$OS_VERSION"
+env
 
-# Container Version
+# Export version
 echo "export C_VERSION=$OS_VERSION" >> $BASH_ENV
+
+env
+
+# Container version
+C_VERSION="$OS_VERSION"
+
+env
 
 echo C_VERSION = $C_VERSION
 

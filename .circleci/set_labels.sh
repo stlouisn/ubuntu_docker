ddf1387809f1f3ad6e1d8b13f5e33a53bc19fc25
stlouisn <stlouisn@hotmail.com>
2022-07-28 13:12:59 -0300

    Update set_labels.sh

--- .circleci/set_labels.sh
+++ .circleci/set_labels.sh
@@ -34,13 +34,13 @@ cat dockerfile
 # Append labels to dockerfile
 cat <<- EOF >> dockerfile
 
-LABEL \
-	org.label-schema.build-date="$BUILD_DATE" \
-	org.label-schema.description="$C_DESCRIPTION" \
-	org.label-schema.maintainer="$C_MAINTAINER" \
-	org.label-schema.name="$C_NAME" \
-	org.label-schema.url="$C_URL" \
-	org.label-schema.version="$C_VERSION" \
+LABEL \\
+	org.label-schema.build-date="$BUILD_DATE" \\
+	org.label-schema.description="$C_DESCRIPTION" \\
+	org.label-schema.maintainer="$C_MAINTAINER" \\
+	org.label-schema.name="$C_NAME" \\
+	org.label-schema.url="$C_URL" \\
+	org.label-schema.version="$C_VERSION" \\
 	org.label-schema.schema-version="$SCHEMA_VERSION"
 EOF
 

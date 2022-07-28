05915591b9bfd9eb6789fd9a0b7e32a66a80d9e7
stlouisn <stlouisn@hotmail.com>
2022-07-28 20:40:47 -0300

    Update get_version - Copy.sh

--- .circleci/get_version - Copy.sh
+++ .circleci/get_version - Copy.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
+find rootfs/. -name '*.sh' -print0 | xargs -0 echo
 find rootfs/. -name '*.sh' -print0 | xargs -0 chmod +x
 
-for FILE in $(find rootfs/. -name "*.sh"); do
-    echo $FILE
-    chmod +x $FILE
-done
+# for FILE in $(find rootfs/. -name "*.sh"); do
+#     echo $FILE
+#     chmod +x $FILE
+# done

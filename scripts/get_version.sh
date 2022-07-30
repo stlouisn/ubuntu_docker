069bf67dafd2845c7bc9a7a778ce8a1bd0686c65
stlouisn <stlouisn@hotmail.com>
2022-07-30 09:17:42 -0300

    Update get_version.sh

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -9,5 +9,4 @@ OS_CODENAME="$(curl -fsSL https://raw.githubusercontent.com/tianon/docker-brew-u
 APP_VERSION="$(curl -fsSL --retry 5 --retry-delay 2 http://releases.ubuntu.com/$OS_CODENAME/ | grep title | awk -F ' ' {'print $2'})"
 
 # Export C_VERSION
-#echo 'export C_VERSION="'$APP_VERSION'"' >> $BASH_ENV
 echo "export C_VERSION=\""$APP_VERSION"\"" >> $BASH_ENV
